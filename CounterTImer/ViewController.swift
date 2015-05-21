45b8b15f3aea567140e178b38f6aa8e93c201da1
sthota <sthota@paypal.com>
2015-05-21 13:54:01 +0800

    Code

--- CounterTImer/ViewController.swift
+++ CounterTImer/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CounterLabel: UILabel!
+    var Counter : Int = 0
+    var Timer : NSTimer = NSTimer()
+    
+    
     override func viewDidLoad() {
+        
+        CounterLabel.text = String(Counter)
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +29,22 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func StartCounter(sender: AnyObject) {
+        Timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("UpdateCounter"), userInfo: nil, repeats: true)
+    }
+    
+    func UpdateCounter(){
+        CounterLabel.text = String(Counter++)
+    }
+    
+    @IBAction func PauseCounter(sender: AnyObject) {
+        Timer.invalidate()
+    }
+    
+    @IBAction func ResetCounter(sender: AnyObject) {
+        Timer.invalidate()
+        Counter = 0
+        CounterLabel.text = String(Counter)
+    }
 }
 
